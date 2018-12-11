16b03ce3d0426bad46593cb3900d9f3d4ea8960b
Awtian <me@awtian.com>
2018-12-11 16:40:56 +0700

    initial commit

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import {Table, Button} from 'reactstrap'
+import axios from 'axios'
+
+import Contact from './Contact'
+import Modal from './Modal'
+
+class ContactList extends Component {
+  state = {
+    contacts: [],
+    showModal: false,
+    editMode: false,
+    data: {
+      _id: '',
+      name: '',
+      phone: '',
+      company: ''
+    }
+  }
+
+  toggleModal = () => {
+    this.setState({
+      showModal: !this.state.showModal,
+      editMode: false,
+      data: {
+        _id: '',
+        name: '',
+        phone: '',
+        company: ''
+      }
+    })
+  }
+
+  toggleEdit = (data) => {
+    this.setState({
+      showModal: !this.state.showModal,
+      editMode: true,
+      data: data
+    })
+  }
+
+  handleChanges = (e) => {
+    this.setState({
+      data: {
+        ...this.state.data,
+        [e.target.name] : e.target.value
+      }
+    })
+  }
+
+  fetchContacts = () => {
+    axios.get('http://localhost:3000/contacts/')
+    .then(({data}) => this.setState({
+      contacts: data
+    }))
+    .catch(err => {console.log(err)})
+  }
+
+  addContacts = () => {
+    this.toggleModal()
+    axios.post('http://localhost:3000/contacts/', {
+      name: this.state.data.name,
+      phone: this.state.data.phone,
+      company: this.state.data.company,
+    })
+    .then(({data}) => this.setState({
+      data: {
+        _id: '',
+        name: '',
+        phone: '',
+        company: ''
+      }
+    }, () => {this.fetchContacts()} ))
+    .catch(err => {console.log(err)})
+  }
+
+  deleteContacts = (id) => {
+    axios.delete(`http://localhost:3000/contacts/${id}`)
+      .then(({data}) => {
+        alert('data has been deleted')
+        this.fetchContacts()
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  updateContacts = () => {
+    this.toggleModal()
+    axios.put(`http://localhost:3000/contacts/${this.state.data._id}`, {
+      name: this.state.data.name,
+      phone: this.state.data.phone,
+      company: this.state.data.company,
+    })
+    .then(({data}) => this.setState({
+      data: {
+        _id: '',
+        name: '',
+        phone: '',
+        company: ''
+      }
+    }, () => {this.fetchContacts()} ))
+    .catch(err => {console.log(err)})
+  }
+
+
+  componentDidMount = () => {
+    this.fetchContacts()
+  }
+
+  render() {
+    const { contacts, showModal } = this.state
+    return (
+      <div style={{marginTop: '3vh'}} className="container">
+        <Button style={{margin: '10px', padding: '20px'}} color="primary" onClick={() => this.toggleModal()}> Add New Contact</Button>
+        <Table dark>
+          <tr>
+            <th>Name</th>
+            <th>Phone</th> 
+            <th>Company</th>
+            <th>Active</th>
+            <th>Actions</th>
+          </tr>
+          {contacts.map(each => <Contact contact={each} delcon={this.deleteContacts} toggleEdit={this.toggleEdit}/>)}
+        </Table>
+        <Modal 
+          showModal={showModal}
+          toggle={this.toggleModal}
+          data={this.state.data}
+          handleChanges={this.handleChanges}
+          add={this.addContacts}
+          edit={this.state.editMode}
+          update={this.updateContacts}
+          />
+      </div>
+    );
+  }
+}
+
+export default ContactList;
